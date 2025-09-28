aaddfa470cf9cbf1915dd15acc430c67773b6310
Ichen-ai <22ivanchen@gmail.com>
2025-09-28 05:49:27 -0400

    added KarmaTracker
    Tracks what choice the player just made
    
    Sometimes the words don't match up to what actually happens

--- karma/Scripts/main.gd
+++ karma/Scripts/main.gd
@@ -7,6 +7,7 @@ extends Node2D
 @onready var label: Label = $Button/Label
 @onready var label_2: Label = $Button2/Label2
 @onready var audio_stream_player_3: AudioStreamPlayer = $AudioStreamPlayer3
+@onready var karmaT: Label = $KarmaTraacker
 
 #current monitor
 var curMonitor = 1
@@ -48,46 +49,63 @@ func _on_button_button_up() -> void:
 		if curJP == "Bullied":
 			if randint <= 6:
 				Main.jakeKarma += 10
+				karmaT.text = "Beat up:|\n+10 Karma"
 			else:
 				Main.jakeKarma -= 15
+				karmaT.text = "Saved by \nTeacher!\n-15 Karma"
 		if curJP == "Project":
 			if randint < 7:
 				Main.jakeKarma -= 10
+				karmaT.text = "Got work done!\n-10 Karma"
 			elif randint <= 8:
 				Main.jakeKarma += 10
+				karmaT.text = "Angry Parent!\n+10 Karma"
 			else:
-				Main.jakeKarma += 10
+				Main.jakeKarma += 15
+				karmaT.text = "Boot out club!\n+15 Karma"
 		if curJP == "Test":
 			if randint <= 8:
 				Main.jakeKarma += 20
+				karmaT.text = "Got Sick!\n+20 Karma"
 			else:
 				Main.jakeKarma -= 5
+				karmaT.text = "Winged it!\n-5 Karma"
 	if Main.curMonitor == 2:
 		if curSP == "Grief":
 			Main.susanKarma += 5
+			karmaT.text = "Sad.\n+5 Karma"
 		if curSP == "Gossip":
 			if randint <= 5:
 				Main.susanKarma -= 10
+				karmaT.text = "They apologized.\n-10 Karma"
 			else:
 				Main.susanKarma += 20
+				karmaT.text = "They fight back,\n+20 Karma"
 		if curSP == "Dinner":
 			Main.susanKarma += 5
+			karmaT.text = "No socializing.\n+5 Karma"
 	if Main.curMonitor == 3:
 		if curBP == "starving":
 			if randint <= 5:
 				Main.billyKarma -= 10
+				karmaT.text = "Success!\n-10 Karma"
 			else:
 				Main.billyKarma += 10
+				karmaT.text = "No charity:\n+10 Karma"
 		if curBP == "Strong":
 			if randint <= 5:
 				Main.billyKarma += 15
+				karmaT.text = "Used all\nenergy!\n+15 Karma"
 			else:
 				Main.billyKarma += 20
+				karmaT.text = "Arrested!\n+20 Karma"
 		if curBP == "Money":
 			if randint <= 9:
 				Main.billyKarma += 5
+				karmaT.text = "What did\nu expect?\n+5 Karma"
 			else:
 				Main.billyKarma -= 30
+				karmaT.text = "TREASURE!!!\n-30 Karma"
 		
 	dayText()
 	Main.curJP = JakePrompt[rng.randi_range(0, 2)]
@@ -107,49 +125,66 @@ func _on_button_2_button_up() -> void:
 		if curJP == "Bullied":
 			if randint <= 6:
 				Main.jakeKarma -= 15
+				karmaT.text = "Defeat bullies!\n-15 Karma"
 			else:
 				Main.jakeKarma += 20
+				karmaT.text = "Gets beat up\nat school \nand at\nthe dojo\n+20 Karma"
 		if curJP == "Project":
 			if randint <= 8:
 				Main.jakeKarma += 10
+				karmaT.text = "Sleep Deprived.\n+10 Karma"
 			else:
 				Main.jakeKarma -= 10
+				karmaT.text = "Works out,\n-10 Karma"
 		if curJP == "Test":
 			if randint <= 7:
 				Main.jakeKarma -= 10
+				karmaT.text = "Pass A+.\n-10 Karma"
 			else:
 				Main.jakeKarma += 10
+				karmaT.text = "Fail anyway.\n+10 Karma"
 	if Main.curMonitor == 2:
 		if curSP == "Grief":
 			if randint <= 8:
 				Main.susanKarma -= 5
+				karmaT.text = "Sucess!\n-5 Karma"
 			elif randint <= 9:
 				Main.susanKarma += 20
+				karmaT.text = "Got hurt.\n+20 Karma"
 			else:
 				Main.susanKarma += 5
+				karmaT.text = "didn't work.\n+5 Karma"
 		if curSP == "Gossip":
-			pass
+			karmaT.text = "Gossip is not\nvery effective"
 		if curSP == "Dinner":
 			if randint <= 5:
 				Main.susanKarma -= 10
+				karmaT.text = "Enjoy company,\n-10 Karma"
 			else:
 				Main.susanKarma += 10
+				karmaT.text = "Hate company,\n+10 Karma"
 	if Main.curMonitor == 3:
 		if curBP == "starving":
 			if randint <= 7:
 				Main.billyKarma -= 15
+				karmaT.text = "Grow Healthy!\n-15 Karma"
 			else:
 				Main.billyKarma += 15
+				karmaT.text = "Waste money.\n+15 Karma"
 		if curBP == "Strong":
 			if randint <= 9:
 				Main.billyKarma -= 20
+				karmaT.text = "Gets Paid!\n-20 Karma"
 			else:
 				Main.billyKarma += 5
+				karmaT.text = "Gets injured,\n+5 Karma"
 		if curBP == "Money":
 			if randint <= 8:
 				Main.billyKarma -= 20
+				karmaT.text = "Success!\n-20 Karma"
 			else:
 				Main.billyKarma += 10
+				karmaT.text = "Fail!\n+10 Karma"
 	dayText()
 	Main.curJP = JakePrompt[rng.randi_range(0, 2)]
 	Main.curSP = SusanPrompt[rng.randi_range(0, 2)]
