633ac8f4296f19c53c2fd0ae69fda09c9f2fdd8c
Ichen-ai <22ivanchen@gmail.com>
2025-09-28 00:47:08 -0400

    BUTTTONSNS

--- karma/Scripts/main.gd
+++ karma/Scripts/main.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 @onready var sprite_2d: AnimatedSprite2D = $Button/AnimatedSprite2D
-@onready var label: Label = $Button/Label
+@onready var sprite2_2d: AnimatedSprite2D = $Button2/AnimatedSprite2D
 
-var jakeKarma = 75
-var susanKarma = 75
+@onready var label: Label = $Button/Label
+@onready var label_2: Label = $Button2/Label2
+
+var jakeKarma = 25
+var susanKarma = 50
 var billyKarma = 75
 
 var greenButtonX = -129
@@ -27,4 +30,11 @@ func _on_button_button_down() -> void:
 func _on_button_button_up() -> void:
 	sprite_2d.play("Idle")
 	label.position = Vector2(16, 0)
-	
+
+func _on_button_2_button_down() -> void:
+	sprite2_2d.play("Press2")
+	label_2.position = Vector2(16,2.3)
+
+func _on_button_2_button_up() -> void:
+	sprite2_2d.play("Idle2")
+	label_2.position = Vector2(16, -1)
