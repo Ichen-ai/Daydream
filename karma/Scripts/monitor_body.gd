229f56729e8e27d42c0b85c5c96ff44fd1795ba9
Kamedeco <kansonton344@gmail.com>
2025-09-28 03:14:38 -0400

    Added audio

--- karma/Scripts/monitor_body.gd
+++ karma/Scripts/monitor_body.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @onready var monitor: AnimatedSprite2D = $Monitor
+@onready var swtchSound = $AudioStreamPlayer2D
+@onready var audio_stream_player_2: AudioStreamPlayer = $"../AudioStreamPlayer2"
 
 var jakeSaved = false
 var susanSaved = false
@@ -10,20 +12,25 @@ var billy = false
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("button1"):
+		audio_stream_player_2.play()
 		monitor.play("Monitor 1")
 	if Input.is_action_just_pressed("button2"):
+		audio_stream_player_2.play()
 		monitor.play("Monitor 2")
 	if Input.is_action_just_pressed("button3"):
+		audio_stream_player_2.play()
 		monitor.play("Monitor 3")
 	if Input.is_action_just_pressed("button4"):
+		audio_stream_player_2.play()
 		monitor.play("Monitor 4")
 	if Input.is_action_just_pressed("button5"):
+		audio_stream_player_2.play()
 		monitor.play("Monitor 5")
 	if Input.is_action_just_pressed("button6"):
+		audio_stream_player_2.play()
 		monitor.play("Monitor 6")
 		
 		
