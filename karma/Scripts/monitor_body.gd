cb0988652c07d5d7a5eb776541c234d932f3f076
Ichen-ai <22ivanchen@gmail.com>
2025-09-27 20:03:50 -0400

    Deleted print statement

--- karma/Scripts/monitor_body.gd
+++ karma/Scripts/monitor_body.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 @onready var monitor: AnimatedSprite2D = $Monitor
+var jakeSaved = false
+var susanSaved = false
+var billy
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,10 +14,8 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("button1"):
 		monitor.play("Monitor 1")
-		print("button1 pressed")
 	if Input.is_action_just_pressed("button2"):
 		monitor.play("Monitor 2")
-		print("button2 pressed")
 	if Input.is_action_just_pressed("button3"):
 		monitor.play("Monitor 3")
 	if Input.is_action_just_pressed("button4"):
@@ -23,3 +24,5 @@ func _process(_delta: float) -> void:
 		monitor.play("Monitor 5")
 	if Input.is_action_just_pressed("button6"):
 		monitor.play("Monitor 6")
+		
+		
